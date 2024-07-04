6ae9df07aa66d20f414d5d09edb3d60f9b509680
HimanshuGolyan01 <golyanhimanshu@gmail.com>
2024-07-04 11:28:38 +0530

    Login api

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const router = express.Router()
 
 
@@ -29,6 +30,27 @@ router.post('/signup', function(req,res){
                                         res.status(200).json({msg : "user added "})
                                     }) 
                               })
+                }
+            })
+    }
+})
+
+router.post('/login',function(req,res){
+    const {email,password} = req.body
+    if(!email || !password) {
+        res.status(400).json({ msg : "All fields are required"})
+    }
+    else {
+        User.findOne({email :email})
+            .then((userExist) => {
+                if(!userExist) {
+                    res.status(422).json({error : "Invalid Email"})
+                }
+                else {
+                    const uId = userExist._id
+                   const token = jwt.sign({id : uId},process.env.SECRET_KEY)
+                   res.status(200).json({msg : "user logged in successfully" , token})
+
                 }
             })
     }
